d449e0581172f678c25a2e82928c7dbb4e9333bc
Xiangyi  Luo <u6162693@anu.edu.au>
2021-10-24 16:12:46 +0800

    add stock selection directory

--- trading_strategy/data_preprocessing.py
+++ trading_strategy/data_preprocessing.py
@@ -1,4 +1,29 @@
+"""
+Author: Luo Xiangyi
+Create on: 2021-10-24
 
-
+This module does data preprocessing and feature selection.
+"""
 # RFE blog
 # https://machinelearningmastery.com/rfe-feature-selection-in-python/
+
+import pandas as pd
+import trading_strategy.data_crawler as crawler
+
+# TODO: price prediction or classification ?
+
+
+def labelling_trends(df, lag: int):
+    """
+    Label the price trend data, for classification task.
+    """
+    df[f'trend_lag_{lag}'] = 0
+
+
+if __name__ == "__main__":
+
+    for stock in crawler.SELECTED_STOCKS:
+
+        combined_df = pd.read_csv(f'../trading_strategy_data/combined_data/{stock}_combined_data.csv')
+        for n in range(10):
+            labelled_df = labelling_trends(combined_df, n)
